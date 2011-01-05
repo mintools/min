f4b98333c4d7116066626570fe7820d591b137c3
Stephen O'Young <soyoung@Aizen.local>
2011-01-05 17:53:24 +1100

    Tag Grouping

--- min/app/models/TagGroup.java
+++ min/app/models/TagGroup.java
@@ -2,6 +2,7 @@ package models;
 
 import play.db.jpa.Model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
@@ -15,6 +16,10 @@ import java.util.List;
 public class TagGroup extends Model {
     public String name;
 
-    @OneToMany(mappedBy = "group")
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL)
     public List<Tag> tags = new ArrayList<Tag>();
+
+    public String toString() {
+        return this.name;
+    }
 }
