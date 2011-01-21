330c672817754ccd194b3fbe6ee1de154ea24fe8
Stephen O'Young <soyoung@Aizen.local>
2011-01-21 14:24:48 +1100

    fixed unit tests

--- min/test/TaskIndexTest.java
+++ min/test/TaskIndexTest.java
@@ -13,7 +13,7 @@ import java.util.List;
  * User: soyoung
  * Date: Jan 21, 2011
  */
-public class TasksControllerTest extends UnitTest {
+public class TaskIndexTest extends UnitTest {
     @Before
     public void setup() throws Exception {
         // test.yml is already loaded by Bootstrap.java
@@ -23,50 +23,50 @@ public class TasksControllerTest extends UnitTest {
     @Test
     public void testFilter_noTagInGroup() throws Exception {
         // find all tasks with no tags in group 1
-        List<Task> matchingTasks = Tasks.filterTasks (null, null, new String[]{"1"}, null, null, null);
+        List<Task> matchingTasks = TaskIndex.filterTasks (null, null, new String[]{"1"}, null, null, null);
 
         assertEquals(2, matchingTasks.size());
-        assertTrue(matchingTasks.get(0).id == 3);
-        assertTrue(matchingTasks.get(1).id == 4);
+        assertTrue(matchingTasks.get(0).id == 4);
+        assertTrue(matchingTasks.get(1).id == 3);
 
         // find all tasks with no tags in group 2
-        matchingTasks = Tasks.filterTasks (null, null, new String[]{"2"}, null, null, null);
+        matchingTasks = TaskIndex.filterTasks (null, null, new String[]{"2"}, null, null, null);
 
         assertEquals(5, matchingTasks.size());
-        assertTrue(matchingTasks.get(0).id == 1);
-        assertTrue(matchingTasks.get(1).id == 3);
+        assertTrue(matchingTasks.get(0).id == 6);
+        assertTrue(matchingTasks.get(1).id == 5);
         assertTrue(matchingTasks.get(2).id == 4);
-        assertTrue(matchingTasks.get(3).id == 5);
-        assertTrue(matchingTasks.get(4).id == 6);
+        assertTrue(matchingTasks.get(3).id == 3);
+        assertTrue(matchingTasks.get(4).id == 1);
     }
 
     @Test
     public void testFilter_checkedTags() throws Exception {
         // find all tasks with tag1
-        List<Task> matchingTasks = Tasks.filterTasks (new String[]{"1"}, null, null, null, null, null);
+        List<Task> matchingTasks = TaskIndex.filterTasks (new String[]{"1"}, null, null, null, null, null);
 
         assertEquals(3, matchingTasks.size());
-        assertTrue(matchingTasks.get(0).id == 1);
+        assertTrue(matchingTasks.get(0).id == 6);
         assertTrue(matchingTasks.get(1).id == 2);
-        assertTrue(matchingTasks.get(2).id == 6);
+        assertTrue(matchingTasks.get(2).id == 1);
     }
 
     @Test
     public void testFilter_checkedTags_twoInSameGroup() throws Exception {
         // find all tasks with tag1 OR tag2
-        List<Task> matchingTasks = Tasks.filterTasks (new String[]{"1", "2"}, null, null, null, null, null);
+        List<Task> matchingTasks = TaskIndex.filterTasks (new String[]{"1", "2"}, null, null, null, null, null);
 
         assertEquals(4, matchingTasks.size());
-        assertTrue(matchingTasks.get(0).id == 1);
-        assertTrue(matchingTasks.get(1).id == 2);
-        assertTrue(matchingTasks.get(2).id == 5);
-        assertTrue(matchingTasks.get(3).id == 6);
+        assertTrue(matchingTasks.get(0).id == 6);
+        assertTrue(matchingTasks.get(1).id == 5);
+        assertTrue(matchingTasks.get(2).id == 2);
+        assertTrue(matchingTasks.get(3).id == 1);
     }
 
     @Test
     public void testFilter_checkedTags_twoInDifferentGroups() throws Exception {
         // find all tasks with tag1 AND tag13 (since they are in different groups)
-        List<Task> matchingTasks = Tasks.filterTasks (new String[]{"1", "13"}, null, null, null, null, null);
+        List<Task> matchingTasks = TaskIndex.filterTasks (new String[]{"1", "13"}, null, null, null, null, null);
 
         assertTrue(matchingTasks.get(0).id == 6);
     }
@@ -74,7 +74,7 @@ public class TasksControllerTest extends UnitTest {
     @Test
     public void testFilter_checkedTags_twoInDifferentGroups_twoInSameGroup() throws Exception {
         // find all tasks with tag1 AND tag13 (since they are in different groups)
-        List<Task> matchingTasks = Tasks.filterTasks (new String[]{"1", "2", "13"}, null, null, null, null, null);
+        List<Task> matchingTasks = TaskIndex.filterTasks (new String[]{"1", "2", "13"}, null, null, null, null, null);
 
         assertEquals(1, matchingTasks.size());
         assertTrue(matchingTasks.get(0).id == 6);
@@ -83,7 +83,7 @@ public class TasksControllerTest extends UnitTest {
     @Test
     public void testFilter_searchText() throws Exception {
         // find all tasks with "lucene" in the content
-        List<Task> matchingTasks = Tasks.filterTasks (null, "lucene", null, null, null, null);
+        List<Task> matchingTasks = TaskIndex.filterTasks (null, "lucene", null, null, null, null);
 
         assertEquals(1, matchingTasks.size());
         assertTrue(matchingTasks.get(0).id == 4);
@@ -92,7 +92,7 @@ public class TasksControllerTest extends UnitTest {
     @Test
     public void testFilter_searchText_checkedTags_false() throws Exception {
         // find all tasks with "lucene" in the content And with tag1
-        List<Task> matchingTasks = Tasks.filterTasks (new String[]{"1"}, "lucene", null, null, null, null);
+        List<Task> matchingTasks = TaskIndex.filterTasks (new String[]{"1"}, "lucene", null, null, null, null);
 
         assertEquals(0, matchingTasks.size());
     }
@@ -100,7 +100,7 @@ public class TasksControllerTest extends UnitTest {
     @Test
     public void testFilter_searchText_checkedTags_true() throws Exception {
         // find all tasks with "lucene" in the content And with tag1
-        List<Task> matchingTasks = Tasks.filterTasks (new String[]{"13"}, "lucene", null, null, null, null);
+        List<Task> matchingTasks = TaskIndex.filterTasks (new String[]{"13"}, "lucene", null, null, null, null);
 
         assertEquals(1, matchingTasks.size());
         assertTrue(matchingTasks.get(0).id == 4);
@@ -109,17 +109,17 @@ public class TasksControllerTest extends UnitTest {
     @Test
     public void testFilter_searchText_OR() throws Exception {
         // find all tasks with "lucene" OR "group" in the content
-        List<Task> matchingTasks = Tasks.filterTasks (null, "lucene group", null, null, null, null);
+        List<Task> matchingTasks = TaskIndex.filterTasks (null, "lucene group", null, null, null, null);
 
         assertEquals(2, matchingTasks.size());
-        assertTrue(matchingTasks.get(0).id == 4);
-        assertTrue(matchingTasks.get(1).id == 5);
+        assertTrue(matchingTasks.get(0).id == 5);
+        assertTrue(matchingTasks.get(1).id == 4);
     }
 
     @Test
     public void testFilter_searchText_AND() throws Exception {
         // find all tasks with "lucene" AND "group" in the content
-        List<Task> matchingTasks = Tasks.filterTasks (null, "lucene AND group", null, null, null, null);
+        List<Task> matchingTasks = TaskIndex.filterTasks (null, "lucene AND group", null, null, null, null);
 
         assertEquals(0, matchingTasks.size());
     }
@@ -127,24 +127,24 @@ public class TasksControllerTest extends UnitTest {
     @Test
     public void testFilter_raisedBy() throws Exception {
         // find all tasks owned by member1
-        List<Task> matchingTasks = Tasks.filterTasks (null, null, null, new String[]{"1"}, null, null);
+        List<Task> matchingTasks = TaskIndex.filterTasks (null, null, null, new String[]{"1"}, null, null);
 
         assertEquals(2, matchingTasks.size());
-        assertTrue(matchingTasks.get(0).id == 1);
-        assertTrue(matchingTasks.get(1).id == 2);
+        assertTrue(matchingTasks.get(0).id == 2);
+        assertTrue(matchingTasks.get(1).id == 1);
     }
 
     @Test
     public void testFilter_raisedBy_checkedTags() throws Exception {
         // find all tasks owned by member1 and has tag1
-        List<Task> matchingTasks = Tasks.filterTasks (new String[]{"1"}, null, null, new String[]{"1"}, null, null);
+        List<Task> matchingTasks = TaskIndex.filterTasks (new String[]{"1"}, null, null, new String[]{"1"}, null, null);
 
         assertEquals(2, matchingTasks.size());
-        assertTrue(matchingTasks.get(0).id == 1);
-        assertTrue(matchingTasks.get(1).id == 2);
+        assertTrue(matchingTasks.get(0).id == 2);
+        assertTrue(matchingTasks.get(1).id == 1);
 
         // find all tasks owned by member1 and has tag4
-        matchingTasks = Tasks.filterTasks (new String[]{"4"}, null, null, new String[]{"1"}, null, null);
+        matchingTasks = TaskIndex.filterTasks (new String[]{"4"}, null, null, new String[]{"1"}, null, null);
 
         assertEquals(1, matchingTasks.size());
         assertTrue(matchingTasks.get(0).id == 2);
@@ -153,7 +153,7 @@ public class TasksControllerTest extends UnitTest {
     @Test
     public void testFilter_allYouCanEat() throws Exception {
         // find all tasks that match: has tag1, has "lucene" in content, OR does not have tags in group1, created by Member2
-        List<Task> matchingTasks = Tasks.filterTasks (new String[]{"1"}, "lucene", new String[]{"1"}, new String[]{"2"}, null, null);
+        List<Task> matchingTasks = TaskIndex.filterTasks (new String[]{"1"}, "lucene", new String[]{"1"}, new String[]{"2"}, null, null);
 
         assertEquals(1, matchingTasks.size());
         assertTrue(matchingTasks.get(0).id == 4);
