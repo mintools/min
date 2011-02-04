f703f9a3388cdb64982571695f2a408fb2bf2986
Stephen O'Young <soyoung@Aizen.local>
2011-02-04 13:09:40 +1100

    Removed taskinterests

--- test/MemberTest.java
+++ test/MemberTest.java
@@ -14,24 +14,4 @@ public class MemberTest extends UnitTest {
     public void setup() {
         Fixtures.load("data.yml");
     }
-
-    @Test
-    public void testAddTaskInterest() {
-        Member member = Member.findById(1l);
-        Task task = Task.findById(1l);
-
-        member.addInterest(task);
-
-        assertSame(member.taskInterests.size(), 1);
-    }
-
-    @Test
-    public void testRemoveTaskInterest() {
-        Member member = Member.findById(1l);
-        Task task = Task.findById(1l);
-
-        member.removeInterest(task);
-
-        assertSame(member.taskInterests.size(), 0);
-    }
 }
