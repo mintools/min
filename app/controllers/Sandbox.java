72715f145daa5a30a2f1aabb02627c167f1e61da
365shutters <soyoung@soyabean.com.au>
2011-02-09 11:44:59 +1100

    added first cut of querying audit data

--- app/controllers/Sandbox.java
+++ app/controllers/Sandbox.java
@@ -1,16 +1,21 @@
 package controllers;
 
+import models.Tag;
 import models.Task;
 import org.hibernate.Session;
 import org.hibernate.envers.AuditReader;
 import org.hibernate.envers.AuditReaderFactory;
+import org.hibernate.envers.RevisionType;
 import org.hibernate.envers.query.AuditEntity;
 import org.hibernate.envers.query.AuditQuery;
+import play.db.DB;
 import play.db.jpa.JPA;
 import play.mvc.Controller;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -30,18 +35,11 @@ public class Sandbox extends Controller {
         renderText("DONE");
     }
 
-    public static void testEnvers(Long taskId) throws Exception {
+    public static void getRevisions(Long taskId) throws Exception {
         Session session = (Session) JPA.em().getDelegate();
-
-
-//        AuditReader reader = AuditReaderFactory.get(session);
-//        Task t = (Task) reader.find(Task.class, new Long(1), 1);
-
-
 
         AuditReader reader = AuditReaderFactory.get(session);
         AuditQuery query = reader.createQuery().forRevisionsOfEntity(Task.class, false, true);
-
 
         List auditResult = query.addOrder(AuditEntity.revisionNumber().desc()).add(AuditEntity.id().eq(taskId)).getResultList();
 
@@ -54,5 +52,102 @@ public class Sandbox extends Controller {
         }
 
         render(auditResult);
+    }
+
+    public static void getChanges(Integer revision, Long taskId) throws Exception {
+        Session session = (Session) JPA.em().getDelegate();
+
+        List changeList = new ArrayList();
+
+        AuditReader reader = AuditReaderFactory.get(session);
+        AuditQuery query = reader.createQuery().forEntitiesAtRevision(Task.class, revision).add(AuditEntity.id().eq(taskId));
+        Task currentTaskRevision = (Task) query.getSingleResult();
+
+        query = reader.createQuery().forEntitiesAtRevision(Task.class, revision - 1);
+        Task previousTaskRevision = (Task) query.getSingleResult();
+
+        if (!currentTaskRevision.title.equals(previousTaskRevision.title)) {
+            changeList.add(new Delta("Task", 1, "title"));
+        }
+
+        if (!currentTaskRevision.content.equals(previousTaskRevision.content)) {
+            changeList.add(new Delta("Task", 1, "content"));
+        }
+
+
+        if (currentTaskRevision.assignedTo == null && previousTaskRevision.assignedTo == null) {
+            // no change
+        }
+        else if (currentTaskRevision.assignedTo == null && previousTaskRevision.assignedTo != null) {
+            changeList.add(new Delta("assignedTo", 2, previousTaskRevision.assignedTo.username));
+        }
+        else if (currentTaskRevision.assignedTo != null && previousTaskRevision.assignedTo == null) {
+            changeList.add(new Delta("assignedTo", 0, currentTaskRevision.assignedTo.username));
+        }
+        else if (!currentTaskRevision.assignedTo.equals(previousTaskRevision.assignedTo)) {
+            changeList.add(new Delta("assignedTo", 1, "from " + previousTaskRevision.assignedTo.username + " to " + currentTaskRevision.assignedTo.username));
+        }
+        
+        ResultSet rs = DB.executeQuery("select * from Task_Tag_AUD where REV = " + revision + " and Task_id = " + taskId);
+
+        while (rs.next()) {
+            changeList.add(new Delta("Tag", rs.getInt("REVTYPE"), rs.getString("tags_id")));
+        }
+
+        rs = DB.executeQuery("select * from WorkingOn_AUD where REV = " + revision + " and Task_id = " + taskId);
+
+        while (rs.next()) {
+            changeList.add(new Delta("Workers", rs.getInt("REVTYPE"), rs.getString("member_id")));
+        }
+
+        rs = DB.executeQuery("select * from Comment_AUD where REV = " + revision + " and Task_id = " + taskId);
+
+        while (rs.next()) {
+            changeList.add(new Delta("Comment", rs.getInt("REVTYPE"), rs.getString("content")));
+        }
+
+        rs = DB.executeQuery("select * from Attachment_AUD where REV = " + revision + " and Task_id = " + taskId);
+
+        while (rs.next()) {
+            changeList.add(new Delta("Attachment", rs.getInt("REVTYPE"), rs.getString("title")));
+        }
+
+        render(currentTaskRevision, previousTaskRevision, changeList);
+    }
+
+    public static class Delta {
+        private String entity;
+        private String changeType;
+        private String attribute;
+
+        public Delta(String entity, Integer changeType, String attribute) {
+            this.entity = entity;
+
+            switch (changeType) {
+                case 0:
+                    this.changeType = "Added";
+                    break;
+                case 1:
+                    this.changeType = "Modified";
+                    break;
+                case 2:
+                    this.changeType = "Deleted";
+                    break;
+            }
+
+            this.attribute = attribute;
+        }
+
+        public String getEntity() {
+            return entity;
+        }
+
+        public String getChangeType() {
+            return changeType;
+        }
+
+        public String getAttribute() {
+            return attribute;
+        }
     }
 }
