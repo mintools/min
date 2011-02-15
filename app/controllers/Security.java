0f6c88d985ceabde2ce49fe9f608963aade04083
Stephen O'Young <soyoung@soyabean.com.au>
2011-02-15 17:09:40 +1100

    Fixed nullpointer when run in test mode

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -10,4 +10,8 @@ public class Security extends Secure.Security {
     static boolean authenticate(String username, String password) {
         return Member.connect(username, password) != null;
     }
+
+    static String connected() {
+        return session == null ? null : session.get("username");
+    }
 }
